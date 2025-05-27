7291136a633d5243b338a494cfc4571bc9146403
ReviOrion <husekmartin03@gmail.com>
2025-05-27 21:43:07 +0200

    Delete AlbionISM directory

--- AlbionISM/photon_parser/segmented_packet.py
+++
@@ -1,5 +0,0 @@
-class SegmentedPacket:
-    def __init__(self, hw_Length = 0, bytes_written = 0, gh_Payload = bytearray(0)):
-        self.hw_Length = hw_Length
-        self.bytes_written = bytes_written
-        self.gh_Payload = gh_Payload
--- AlbionISM/photon_parser/uuid_extension.py
+++
@@ -1,4 +0,0 @@
-def uuid_stringify(arr, offset=0) -> str | None:
-    try:
-        return ''.join([[f'{i:02x}' for i in range(256)][arr[offset + i]] + ('-' if i in {3, 5, 7, 9} else '') for i in range(16)]).lower()
-    except: return None
